d86be1578ce63eed383ed64f060b6fb0be29323e
BinhBT <binhbt@sigma-solutions.eu>
2020-05-19 09:51:54 +0700

    init src

---
+++ kafka-example/test.py
@@ -0,0 +1,8 @@
+# from kafkaLib import producer as KP
+from kafkaLib import consumer as KC
+KC.Consumer()
+def run():
+    # Start the Kafka producer
+    # KP.Producer()
+    # Start the Kafka Consumer
+    KC.Consumer()
