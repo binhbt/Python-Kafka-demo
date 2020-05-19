a1e4bc45d3028460129ff943a40403fd1e244336
BinhBT <binhbt@sigma-solutions.eu>
2020-05-19 11:37:42 +0700

    init src

--- kafka-example/kafkaLib/consumer.py
+++ kafka-example/kafkaLib/consumer.py
@@ -3,13 +3,13 @@ from kafka import KafkaConsumer
 
 class Consumer():
     def __init__(self):
+        print('start sub')
         consumer = KafkaConsumer('messages',
                                  bootstrap_servers=['134.209.108.208:9092'],
                                  auto_offset_reset='latest',
                                  enable_auto_commit=True,
                                  auto_commit_interval_ms=500,
                                  consumer_timeout_ms=3000)
-        consumer.subscribe(['messages'])
         for message in consumer:
             print ("Consumed Msg -> '%s' on Topic -> '%s' with Offset -> %d" %
                     (message.value.decode('utf-8'), message.topic, message.offset))
